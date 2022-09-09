7c235285acc24bb7e37fb122e3db1d8ce5bce1c6
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-09 15:46:42 -0700

    Print a-zA-z

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	char aA_zZ
+
+	for (aA_zZ = 'a'; a <= 'z'; aA_zZ++)
+		putchar(aA_zZ);
+	for (aA_zZ = 'A'; a <= 'Z'; aA_zZ++)
+		putchar(aA_zZ);
+	putchar('\n');
+	return (0);
+}
