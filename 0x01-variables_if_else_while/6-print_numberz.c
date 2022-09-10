1a56973dece14e342cc721073332784e7ce800e8
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-09 17:39:14 -0700

    Print 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+	putchar('\n');
+	return (0);
+	}
+}
