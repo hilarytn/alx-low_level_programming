d94092b57a7cda2a2c95e409c35b929f772e628a
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-14 16:55:09 -0700

    Print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,6 +35,13 @@ void print_times_table(int n)
 					_putchar('0' + (prod / 100));
 					_putchar('0' + (prod / 10) % 10);
 					_putchar('0' + (prod % 10));
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
 				}
 			}
 			_putchar('\n');
