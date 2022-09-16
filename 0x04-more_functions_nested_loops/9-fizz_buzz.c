aa449fd3ba3bbf2827c22451442c97cc2de95965
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-16 15:10:06 -0700

    Print Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,41 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - Prints a Fizz Buzz Program
+ * main - Prints a Fizz Buzz program
  *
- * Return: always return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		} 
 		else
 		{
-			printf("%d, i");
+			printf("%d", x);
 		}
-		if (i != 100)
+
+		if (x != 100)
+		{
 			printf(" ");
+		}
 	}
+
 	printf("\n");
+
 	return (0);
 }
-			
