4bfbdf726f58768618259fd5f5cd6b68eb5237f5
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-16 01:06:52 -0700

    Print 1-14 x10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * more_numbers - Print umbers 1-14 10x
+ * more_numbers - Print 1-14 10x
  *
- * Return: 10 times of the numbers since 0-14
+ * Return: 1-14 10x
+ *
  */
 void more_numbers(void)
 {
 	int x;
 	int y;
 
-	for (x - 0; x < 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if(y > 9)
+			if (y > 9)
 			{
 				_putchar((y / 10) + '0');
 			}
