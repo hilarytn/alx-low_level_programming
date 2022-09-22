84b87cad53f6722f9e4bb9f8aa0a3f446b994384
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-22 08:12:42 -0700

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ *
+ * Return: int value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = j = k = diff = 0; 
+	int len;
+
+	while (s1[i])
+	{
+		i++;
+	}
+
+	while (s2[j])
+	{
+		j++;
+	}
+
+	if (i <= j)
+	{
+		len = i;
+	}
+	else
+	{
+		len = j;
+	}
+
+	while (k <= len)
+	{
+		if (s1[k] == s2[k])
+		{
+			k++;
+			continue;
+		}
+		else
+		{
+			diff = s1[k] - s2[k];
+			break;
+		}
+
+		k++;
+	}
+
+	return (diff);
