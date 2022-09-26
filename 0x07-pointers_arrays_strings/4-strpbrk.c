b055202293abb652cbf52a85a1091d134bc4ead5
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-26 14:50:51 -0700

    Locate a substring "needle"

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,25 +9,25 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-		int a = 0, b;
+	int a = 0, b;
 
-			while (s[a])
-					{
-								b = 0;
+	while (s[a])
+	{
+		b = 0;
 
-										while (accept[b])
-													{
-																	if (s[a] == accept[b])
-																					{
-																										s += a;
-																														return (s);
-																																	}
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
 
-																				b++;
-																						}
+			b++;
+		}
 
-												a++;
-													}
+		a++;
+	}
 
-				return ('\0');
+	return ('\0');
 }
