bac3660c17b13ecd2ad950a2c66fc3fd73401d47
Hilary Titus Naor <hilarytn@gmail.com>
2022-09-26 13:46:14 -0700

    Copy n bytes from memory area src to memory area dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+	while  (i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 
 	return (dest);
