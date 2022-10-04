5df2f5d42bca3e2bf9fe653a8977c03cbe2e8094
Hilary Titus Naor <hilarytn@gmail.com>
2022-10-04 03:46:55 -0700

    Create an array of chars, and initialize it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 		return (NULL);
-	t = malloc(sizeof(char) * size);
+	hilary = malloc(sizeof(char) * size);
 	if (hilary == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
